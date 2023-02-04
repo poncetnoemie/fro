caafefc8978d7b88c1aacf812a457e290aea46b1
nogrenouille <noemiegrenouille@gmail.com>
2023-02-04 19:05:28 +0100

    Add "copy" command to fix deploy 404 error

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -7,6 +7,9 @@ const fs = require("fs");
     // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
+    // Copy index.html to 404.html
+    console.log("Copy index > 404.html started...");
+    await execa("npm", ["run", "copy"]);
     // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
